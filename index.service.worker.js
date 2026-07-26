5dfe9084cf36fef728833c41b6269d353113138e
jkschnr <jakubsnicla@gmail.com>
2026-07-26 23:00:44 +0200

    Creation: writable name + tag fields (type any name), preset chips quick-fill

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785082777|1621907';
+const CACHE_VERSION = '1785099643|1540990';
 /** @type {string} */
 const CACHE_PREFIX = 'Postcode Wars-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
