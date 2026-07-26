3c97801fc6084f39e373e2e27d941520d1aff642
jkschnr <jakubsnicla@gmail.com>
2026-07-26 12:56:28 +0200

    Fix greeter: full rectangular portrait (no circle) + tap-anywhere to advance

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785061519|1942661';
+const CACHE_VERSION = '1785063159|1867131';
 /** @type {string} */
 const CACHE_PREFIX = 'Postcode Wars-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
