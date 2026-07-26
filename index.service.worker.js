7fe81d756ac3e149a006fb0928de683a567cbaa9
jkschnr <jakubsnicla@gmail.com>
2026-07-26 18:19:37 +0200

    Character creation v2: WoW-style procedural paperdoll (doll.js ported to GDScript) + Look/Class/Name steps

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785081474|2140766';
+const CACHE_VERSION = '1785082777|1621907';
 /** @type {string} */
 const CACHE_PREFIX = 'Postcode Wars-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
