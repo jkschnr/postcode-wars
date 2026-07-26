b630f7b97f7b3973ac1d8598e1eacce5f8c039b9
jkschnr <jakubsnicla@gmail.com>
2026-07-26 14:01:43 +0200

    Block 2: Collect screen + session-open chain (greeter -> collect -> dailies)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785067061|1682770';
+const CACHE_VERSION = '1785067302|1554370';
 /** @type {string} */
 const CACHE_PREFIX = 'Postcode Wars-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
